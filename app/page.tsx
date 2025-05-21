ffea23cda51af52e43277176b9b4e370922f44c8
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-21 19:30:12 +0600

    still scaffolding

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
-import { Button } from "@/components/ui/button";
-import { Camera } from "lucide-react";
+
 
 export default function Home() {
   return (
-    <div className="h-screen flex items-center justify-center">
-      <Button variant="default"> Default Button</Button>
-      <Button variant='outline' >
-        <Camera />
-      </Button>
-    </div>
+   <main>
+    <header className="max-w-6xl mx-auto px-4 sm:px-8 py-6" >
+
+    </header>
+
+   </main>
   );
 }
