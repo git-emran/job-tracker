a1edd6f5ee3f3eb85523c3695067a0fa9d32ee10
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-22 21:26:31 +0600

    pages scaffolding done

---
+++ app/(dashboard)/jobs/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function JobsPage() {
+  return (
+    <h1 className='text-6xl' >JobsPage</h1>
+  )
+}
+
+export default JobsPage
--- app/page.tsx
+++ app/page.tsx
@@ -12,20 +12,22 @@ export default function Home() {
         <Image src={Logo} alt="Logo" height={30} width={30} />
       </header>
       <section className="max-w-6xl mx-auto px-4 sm:px-8 h-screen -mt-20 grid lg:grid-cols-[1fr, 400px] items-center ">
-        <div className="items-center justify-center text-center gap-6 flex flex-col">
+        <div className="items-center justify-center text-center gap-4 flex flex-col">
           <TextPressure
             text="Track your Job Hunting Progress Easily"
             textColor="#00000"
             minFontSize={24}
           />
           <Text as="div">
-            Job Hunting can be overwhelming, <Strong className="text-purple-500">EZtrack</Strong> makes it
-            easy.
+            Job Hunting can be overwhelming,{" "}
+            <Strong className="text-purple-500">EZtrack</Strong> makes it easy.
           </Text>
 
-          <Button className="cursor-pointer" size="lg" variant="default">
-            Get Started for Free
-          </Button>
+          <a href="/add-job">
+            <Button className="cursor-pointer" size="lg" variant="default">
+              Get Started for Free
+            </Button>
+          </a>
         </div>
       </section>
     </main>
