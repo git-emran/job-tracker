b0b0139ff525ccf524cbff80825b620718945b33
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-24 14:49:59 +0600

    nav scaffolding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ function Navbar() {
   return (
     <nav className="bg-muted py-4 sm:px-15 lg:px-24 px-4 flex items-center justify-between">
       <LinksDropdown />
-      <div className="flex items-center gap-x-4" >
-<ThemeToggle />
-<UserButton />
+      <div className="flex items-center gap-x-4">
+        <ThemeToggle />
+        <UserButton />
       </div>
     </nav>
   );
