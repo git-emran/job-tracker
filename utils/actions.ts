ae97ce2f3095a96f2de7c3f41ca1ad86c52dcaa6
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-05-31 19:35:05 +0600

    schema and render db added

---
+++ utils/actions.ts
@@ -0,0 +1,30 @@
+import prisma from "./db";
+import { auth } from "@clerk/nextjs/server";
+import { JobType, CreateAndEditJobType, createAndEditJobSchema } from "./types";
+import { redirect } from "next/navigation";
+import { Prisma } from "@prisma/client";
+import dayjs from "dayjs";
+
+function authenticateAndRedirect(): string {
+  const { userId } = auth();
+  if (!userId) redirect("/");
+
+  return userId;
+}
+
+export async function createJobAction(
+  values: CreateAndEditJobType
+): Promise<JobType | null> {
+  const userId = authenticateAndRedirect();
+  try {
+    const job: JobType = await prisma.job.create({
+      data: {
+        ...values,
+        clerkID: auth().userId || "",
+      },
+    });
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
